15eaee21bc405c8eec1dd89655e11b6d58e63b27
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-05-06 16:56:52 +0400

    install sass et create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+// Header.js
+import React from "react";
+
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
