d8d67e17c25c21e0ef3fe7100655f9641af3553a
Brico97442 <137392526+Brico97442@users.noreply.github.com>
2024-05-08 05:17:32 +0400

    try to deploying at netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Sidemodal from "./components/Sidemodal";
@@ -12,7 +12,7 @@ import "./styles/index.css";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -23,16 +23,16 @@ function App() {
 
   return (
     <div className="App">
-      <HashRouter>
-        <Header openModal={openModal} />
+      <Router>
+        <Header openModal={openModal} /> 
         <Sidemodal closeModal={closeModal} isOpen={isModalOpen} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Projects" element={<Projects />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
-        <Footer />
-      </HashRouter>
+        <Footer/>
+      </Router>
     </div>
   );
 }
