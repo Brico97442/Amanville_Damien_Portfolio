c94dfd810d2ca1d973d005891fc8db7ec1187c0f
Brico97442 <amanvilledamien@hotmail.fr>
2024-05-14 18:31:08 +0400

    edit style of welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,24 +1,25 @@
 // Header.js
 import React from "react";
 import "../styles/sass/header.scss";
-import damienImg from "../img/assets/img/portfolio/20230703_172611.jpg"
+import damienImg from "../img/assets/img/portfolio/20230703_172611.jpg";
 
 function Welcome() {
-  
   return (
     <section className="welcome">
       <div className="identity">
-        <img src={damienImg} alt="Damien"/>      
+        <img src={damienImg} alt="Damien" />
       </div>
-      <div className="animated-text">
-        <span></span>
-      </div>
-      <div className="welcome-content">
-        <h1>DévelOppeur</h1>
-        <h2>Front-End*</h2>
-        <div className="welcome-subtitle">
-          <span className="undescore"></span>
-          <p>Web</p>
+      <div className="animated">
+        <div className="animated-text">
+          <span></span>
+        </div>
+        <div className="welcome-content">
+          <h1>DévelOppeur</h1>
+          <h2>Front-End*</h2>
+          <div className="welcome-subtitle">
+            <span className="undescore"></span>
+            <p>Web</p>
+          </div>
         </div>
       </div>
     </section>
