73651dc7f84cb33650aaebba4e8a9bb6f4acdae2
Brico97442 <amanvilledamien@hotmail.fr>
2024-05-24 01:05:52 +0400

    edit video card section

--- src/components/Videocard.jsx
+++ src/components/Videocard.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import ReactPlayer from "react-player";
 import CardModal from "./CardModal";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-function Videocard({ content, title, video, description,link }) {
+function Videocard({ content, title, websiteScreenshot, description, link }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -11,26 +10,23 @@ function Videocard({ content, title, video, description,link }) {
 
   const closeModal = () => {
     setIsModalOpen(false);
-    console.log("titi");
   };
-
-  const handleClick = () => {};
 
   return (
     <div className="card" onClick={openModal}>
       <div className="card-video">
         <h2>{title}</h2>
-        <ReactPlayer
-          url={video}
-          playing
-          onStart={handleClick}
-          height="100%"
-          width="100%"
-        />
-        <div className="video-description">
-          {description}
-        </div>
-        {isModalOpen && <CardModal closeModal={closeModal} content={content} link={link} linkText="Visitez le site" icon={faTimes}/>}
+        <img src={websiteScreenshot} alt="illustration du site" />
+        <div className="video-description">{description}</div>
+        {isModalOpen && (
+          <CardModal
+            closeModal={closeModal}
+            content={content}
+            link={link}
+            linkText="Visitez le site"
+            icon={faTimes}
+          />
+        )}
       </div>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 import Videocard from "../components/Videocard";
-import videoData1 from "../img/ninacarducci.webm";
-import videoData2 from "../img/ohmyfood.webm";
+import ninaImg from "../img/nina.png";
+import ohmyfoodImg from "../img/ohmyfood.png";
+import kasaImg from "../img/kasa.png";
+import sophieBluelImg from "../img/sophie_bluel.png";
+import argentBankImg from "../img/argent-bank.png";
+
+const projectData = [
+  {
+    title: "Nina Carducci",
+    description: "Le site d'une photographe",
+    url: ninaImg,
+    content: "Ce site présente le travail de Nina Carducci, une jeune photographe talentueuse et passionnée. Au programme Optimisation SEO, Amélioration des performances du site, et mise en place des métadonnées.",
+    link: "https://brico97442.github.io/NinaCarducci/"
+  },
+  {
+    title: "Oh My Food",
+    description: "Application de Réservation",
+    url: ohmyfoodImg,
+    content: "Oh My Food est un projet qui a été réalisé en Html, CSS, et Javascript. Il s'agit d'une interface utilisateur pour les restaurateurs, afin de publier en ligne leurs menus.",
+    link: "https://brico97442.github.io/OhMyFood/"
+  },
+  {
+    title: "Kasa",
+    description: "Application d'hébergements",
+    url: kasaImg,
+    content: "Kasa est un projet d'application permettant au particulier de réserver un hébergement.",
+    link: "https://brico97442.github.io/OhMyFood/"
+  },
+  {
+    title: "Sophie Bluel Architecte",
+    description: "Portfolio de Sophiue Bluel",
+    url: sophieBluelImg,
+    content: "Ce projet porte sur la conception du portfolio de Sophie Bluel, mise en place d'un système d'authentification. Elaboration d'une fonctionnalité de tris des projets avec la possibilité d'ajouter et de suprimer des projets",
+    link: "https://brico97442.github.io/OhMyFood/"
+  },
+  {
+    title: "Argent Bank",
+    description: "Application Bancaire",
+    url: argentBankImg,
+    content: "Kasa est un projet d'application permettant au particulier de réserver un hébergement.",
+    link: "https://brico97442.github.io/OhMyFood/"
+  },
+];
 
 function Projects() {
   return (
@@ -10,21 +51,17 @@ function Projects() {
       <div className="divider">
         <hr />
       </div>
-      <div className="card-wrrapper">
-        <Videocard
-          title="Nina Carducci"
-          description="Le site d'une photographe"
-          video={videoData1}
-          content="Ce site présente le travail de Nina Carducci, une jeune photographe talentueuse et passionnée. Au programme Optimisation SEO, Amélioration des performances du site, et mise en place des métadonnées."
-          link="https://brico97442.github.io/NinaCarducci/"
-        />
-        <Videocard
-          title="Oh My Food"
-          description="Application de Réservation"
-          video={videoData2}
-          content="Oh My Food est un projet qui à été réalisé en Html, CSS, et Javascript. Il s'agit d'une interface utilisateur pour les restaurateurs, afin de publier en ligne leurs menus."
-          link="https://brico97442.github.io/OhMyFood/"
-        />
+      <div className="card-wrapper">
+        {projectData.map((card, index) => (
+          <Videocard
+            key={index}
+            title={card.title}
+            description={card.description}
+            websiteScreenshot={card.url}
+            content={card.content}
+            link={card.link}
+          />
+        ))}
       </div>
     </main>
   );
